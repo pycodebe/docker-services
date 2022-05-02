25d6f0c58df8bdcdb25a7c509957ff02df3eea80
Olivier <noreply4github@gmail.com>
2022-05-02 22:05:28 +0200

    add Oracle database as a new service

---
+++ docker/dev/database/sql/create_tablespace.sql
@@ -0,0 +1,13 @@
+CREATE TABLESPACE tbs_data 
+   DATAFILE 'tbs_data.dbf' 
+   SIZE 100m;
+
+CREATE TABLESPACE tbs_index 
+   DATAFILE 'tbs_index.dbf' 
+   SIZE 100m;
+
+CREATE TABLESPACE tbs_lob 
+   DATAFILE 'tbs_lob.dbf' 
+   SIZE 100m;
+
+exit;
