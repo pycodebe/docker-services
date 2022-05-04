fa7cd59ddaa291ec4d32b0c6d147f9b437a20f7a
Olivier <noreply4github@gmail.com>
2022-05-04 22:10:30 +0200

    fix the database container - add users and tablespaces

--- docker/dev/database/sql/01_create_tablespaces.sql
+++ docker/dev/database/sql/01_create_tablespaces.sql
@@ -1,13 +1,16 @@
+alter session set "_ORACLE_SCRIPT"=true;
+alter session set container=XEPDB1;
+
 CREATE TABLESPACE tbs_data 
    DATAFILE 'tbs_data.dbf' 
-   SIZE 100m;
+   SIZE 2000m;
 
 CREATE TABLESPACE tbs_index 
    DATAFILE 'tbs_index.dbf' 
-   SIZE 100m;
+   SIZE 1000m;
 
 CREATE TABLESPACE tbs_lob 
    DATAFILE 'tbs_lob.dbf' 
-   SIZE 100m;
+   SIZE 5000m;
 
 exit;
